16764ff867b7e4813763d1b5344b45ccefc6ee1d
Kaique Covo <kaique.kng@gmail.com>
2020-06-14 19:33:19 -0300

    [New] - React Navigation

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View } from 'react-native'
+
+/* Styled Components */
+import * as Styled from './styles'
+
+const SignIn: React.FC = () => <Styled.Container />
+
+export default SignIn
