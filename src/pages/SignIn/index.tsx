449948dec4ba5b457e99cdea326e8dead6c47974
Kaique Covo <kaique.kng@gmail.com>
2020-06-14 23:16:49 -0300

    [New] - Import fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,6 +9,7 @@ import * as Styled from './styles'
 const SignIn: React.FC = () => (
   <Styled.Container>
     <Image source={logo} />
+    <Styled.Title>Faça seu Login</Styled.Title>
   </Styled.Container>
 )
 
